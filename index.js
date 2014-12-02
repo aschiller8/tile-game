709e199376f3b29a8441a7234e7e28da3a117c4f
aschiller8 <aschiller8@hotmail.com>
2014-12-02 14:12:00 -0500

    Added function to end game upon collision

--- index.js
+++ index.js
@@ -34,8 +34,10 @@ function key_pressed_down(event) {
     occupants[protagonist.y][protagonist.x] = protagonist.element;
     render();
 }
-function findProtagonist () {
-	
+function getoutofmyBubble () {
+	if (protagonist.x === enemy.x && protagonist.y === enemy.y) {
+		window.alert("Game Over");
+	}
 	
 }
 
